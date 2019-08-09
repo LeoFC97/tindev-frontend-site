9f4fc9db192377f191a0e66662804a83ce6ec413
Leonardo <leonardo.fc2@outlook.com>
2019-08-09 01:59:52 -0300

    remove unused stuff

---
+++ src/pages/Login.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import logo from '../assets/logo.svg'
+import './Login.css'
+export default function Login(){
+    return (
+        <img src={logo} alt="tindev"/>
+    )
+}
